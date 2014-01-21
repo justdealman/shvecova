0c5080be19291e16f7d7c59cc9e70aaa31b4be0c
Alexandr <justdealman@gmail.com>
2014-01-21 19:21:05 +0700

    original version

---
+++ js/dynamic.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function() {
+	$('.slider').superslides({
+		slide_easing: 'easeInOutCubic',
+		slide_speed: 800,
+		pagination: true,
+		hashchange: false,
+		scrollabe: false,
+		play: 7500
+	});
+	if ( $('.menu .nav li').children('ul').length > 0 ) {
+		$('.menu .nav li a').click(function() {
+			$(this).parent('li').children('ul').slideDown(0);
+			return false;
+		});
+	}
+	$('.menu .nav li ul li.back a').click(function() {
+		$(this).parent('li').parent('ul').slideUp(0);
+		return false;
+	});
+	$('body').append('<div class="fade"></div>');
+	$('.quotes h6 a').click(function() {
+		$('.fade, .popup').fadeIn(150);
+		return false;
+	});
+	$('.fade').click(function() {
+		$('.fade, .popup').fadeOut(150);
+		return false;
+	});
+	$(this).keydown(function(eventObject){
+		if (eventObject.which == 27)
+		$('.fade, .popup').fadeOut(150);
+	});
+});
